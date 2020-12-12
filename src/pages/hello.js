5b8e8e7d7c5f1568175ac4b2ad612e5f10b8f05a
Geoff Schwartz <eieio@pixelsoup.com.au>
2020-12-12 16:27:30 +1100

    hello.js Small change to test gitkraken

--- src/pages/hello.js
+++ src/pages/hello.js
@@ -6,7 +6,7 @@ import Layout from '../components/layout';
 
 const helloPage = props => (
   <Layout>
-    <h1>Hi people</h1>
+    <h1>Hi peoples</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
